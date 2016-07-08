1f96b921ee271fe64942c82fb26a60c8aa57184d
Dominik Lang <dolang@users.noreply.github.com>
2016-07-08 15:44:00 +0200

    Recursion implemented, not conclusively tested.

--- test/main_test.py
+++ test/main_test.py
@@ -19,21 +19,46 @@ class TestMain(unittest.TestCase):
         os.chdir(os.path.join(os.path.dirname(__file__), 'files'))
 
     
-    def test_build_stat(self):
+    def test_build_stats(self):
         cwd = os.getcwd()
-        stats = main.build_stats('b')
-        files = [stat[0] for stat in stats[0]]
-        self.assertFalse(stats[1])
+        stats, errors = main.build_stats('b')
+        files = [stat[0] for stat in stats]
+        self.assertFalse(errors)
         
         file_same = os.path.join(cwd, 'b', 'same')
         file_different = os.path.join(cwd, 'b', 'different')
         self.assertIn(file_same, files)
         self.assertIn(file_different, files)
         
-        file_stats = [stat[1] for stat in stats[0]]
+        file_stats = [stat[1] for stat in stats]
         self.assertTrue(all(type(stat) is tuple for stat in file_stats))
         
-        md5_hashes = [binascii.b2a_hex(stat[2]) for stat in stats[0]]
+        md5_hashes = [binascii.b2a_hex(stat[2]) for stat in stats]
+        self.assertIn(b'2145971cf82058b108229a3a2e3bff35', md5_hashes)
+        self.assertIn(b'bc70bbb8c964b8b105039a3010775e47', md5_hashes)
+    
+    
+    def test__build_stats(self):
+        stats, errors = [], []
+        len_stats, len_errors = len(stats), len(errors)
+        new_stats, new_errors = main._build_stats(os.getcwd(), ['foo'], stats, errors)
+        self.assertIs(stats, new_stats)
+        self.assertIs(errors, new_errors)
+        self.assertEqual(len(new_stats), len_stats + 1)
+        self.assertEqual(len(new_errors), len_errors)
+        self.assertEqual(b'2145971cf82058b108229a3a2e3bff35',
+                         binascii.b2a_hex(new_stats[0][2]))
+    
+    
+    def test__build_stats_recursively(self):
+        stats, errors = [], []
+        len_stats, len_errors = len(stats), len(errors)
+        new_stats, new_errors = main._build_stats_recursively(os.getcwd(), stats, errors)
+        self.assertIs(stats, new_stats)
+        self.assertIs(errors, new_errors)
+        self.assertEqual(len(new_stats), len_stats + 7)
+        self.assertEqual(len(new_errors), len_errors)
+        md5_hashes = [binascii.b2a_hex(stat[2]) for stat in stats]
         self.assertIn(b'2145971cf82058b108229a3a2e3bff35', md5_hashes)
         self.assertIn(b'bc70bbb8c964b8b105039a3010775e47', md5_hashes)
     
